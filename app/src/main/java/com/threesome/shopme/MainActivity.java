714f63d84c53ea504e1db6f296a496d5dbb984dc
LanAnhNghiem <lananh.nghiem0502@gmail.com>
2017-10-23 21:50:35 +0700

    Initial commit

---
+++ app/src/main/java/com/threesome/shopme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.threesome.shopme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
